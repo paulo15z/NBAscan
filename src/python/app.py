26655891addeabfed719dfc2f2012488a655901a
paulo15z <paulociee123@gmail.com>
2025-04-01 17:38:26 -0300

    cheguei em casa

--- src/python/app.py
+++ src/python/app.py
@@ -1,21 +1,35 @@
 # arquivo principal
 
 from nba_api.stats.static import teams
-
-def listar_times():
-    times = teams.get_teams()
-    for time in times:
-        print(f"{time['full_name']} - ID: {time['id']}")
-
-#if __name__ == "__main__":
-    #listar_times()
-
+import sqlite3
+import json
 import subprocess
 
+DB_PATH = "nba_scan.db"
+
+def salvar_dados_brutos(dados):
+    #SALVA OS DADOS >BRUTOS< NO BANCO DE DADOS
+    conexao = sqlite3.connect(DB_PATH)
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+        INSERT INTO raw_data (json_data, timestamp)
+        VALUES (?, datetime('now'))
+ """, (json.dumps(dados),))
+    
+    conexao.commit()
+    conexao.close()
+
+def listar_times():
+    #BUSCA OS TIMES E SALVA RAW NO BANCO DE DADOS
+    times = teams.get_teams()
+    salvar_dados_brutos(times)
+
 def processar_dados():
+    # CHAMA O PROCESSAMENTO.C 
     resultado = subprocess.run(["src/c/processamento"], capture_output=True, text=True)
-    print(resultado.stdout)
+    print("Resultado do processamento:\n", resultado.stdout)
 
 if __name__ == "__main__":
-    processar_dados()
     listar_times()
+    processar_dados()
